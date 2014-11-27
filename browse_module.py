32a18e9f60727492164447f3a5d616b6879bd563
btimo <timo.b35@gmail.com>
2014-11-27 02:01:46 +0100

    - first step with twitch api : games list fetch and channel fetch from a game. - next step : create a gui table to represent the game list

--- browse_module.py
+++ browse_module.py
@@ -1,4 +1,9 @@
-import requests
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests, json
+
+
 
 client_id = "81gaw82lvaj8sbbkrmkxfer26pfczcz"
 
@@ -7,9 +12,50 @@ headers = {'Accept' : 'application/vnd.twitchtv.v2+json', 'Client-ID': client_id
 
 api_root = "https://api.twitch.tv/kraken"
 
+#channel object
+class Channel(object):
+	def __init__(self, game, preview, mature, status, logo, url, display_name, viewers, link):
+		self.game = game
+		# preview img
+		self.preview = preview
+		# mature content ?
+		self.mature = mature
+		# Steam description
+		self.status = status
+		self.logo = logo
+		# for livestreamer ;)
+		self.url = url
+		self.display_name = display_name
+		self.viewers = viewers
+		# link in API to update viewers count, stream description and others.
+		self.link = link
+
+
+# game object
+class Game(object):
+	def __init__(self, name, pic_large, pic_small, pic_medium, channels, viewers):
+		self.name = name
+		self.pic_large = pic_large
+		self.pic_small = pic_small
+		self.pic_medium = pic_medium
+		self.channels = channels
+		self.viewers = viewers
+	
+	def access_game_streams(self):
+		res = make_request(api_root + '/streams', {'game' : self.name, 'limit': 25, 'offset': 0})
+		channel_list = [Channel(elem['game'], elem['preview'], elem['channel']['mature'],
+			elem['channel']['status'], elem['channel']['logo'], elem['channel']['url'],
+			 elem['channel']['display_name'], elem['viewers'], elem['_links']['self']) for elem in res.json()['streams']]
+
+
 def get_games():
-	res = make_request('https://api.twitch.tv/kraken/games/top')
-	return res
+	res = make_request(api_root+'/games/top')
+	game_list = [Game(elem['game']['name'], elem['game']['box']['large'], elem['game']['box']['small'], 
+		elem['game']['box']['medium'], elem['channels'], elem['viewers']) for elem in res.json()['top']]
+
+	next_url =res.json()['_links']['next']
+
+	return game_list, next_url
 
 def get_channels():
 	pass
@@ -21,9 +67,8 @@ def following():
 	pass
 
 
-def make_request(link):
-	print headers
-	return requests.get(link, headers=headers)
+def make_request(link, params=None):
+	return requests.get(link, headers=headers, params=params)
 
 #to get Oauth from user
 # redirect to localhost and listen on localhost:port to get the key for the user ...
@@ -35,7 +80,13 @@ def make_request(link):
 # headers = {'Accept': 'application/vnd.twitchtv[.version]+json', 'Client-id': 'hash' }
 
 def main():
-	print get_games().json()
+	current_games_list, next_games_list = get_games()
+	for elem in current_games_list:
+		print 'game: ' + elem.name.encode('utf-8') + ', channels: ' + str(elem.channels) + ',viewers: '+ str(elem.viewers)
+
+	print next_games_list
+
+	print current_games_list[0].access_game_streams()
 
 if __name__=='__main__':
 	main()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import Tkinter as tk
 import ttk
-import gstreamer_module, livestreamer_module
+import gstreamer_module, livestreamer_module, browse_module
 import gi
 from gi.repository import GObject
 
@@ -12,9 +12,10 @@ class GUI():
         self.root = tk.Tk()
         self.player = gstreamer_module.Player()
         self.stream = livestreamer_module.Stream()
-        self.playable = False
+        self.browsing= True
+        self.watching = False
         self.playing = False
-        self.buildUI()
+        self.defaultUI()
         self.root.mainloop()
 
     # callbacks
@@ -34,44 +35,101 @@ class GUI():
         pass
 
 
+    def browse_games(self):
+        #get games list and link for next list
+        games_list, next_list = browse_module.get_games()
+        # create a table
 
-    def search(self):
-        # send streamer name to the livestreamer module
-        name = self.streamer_entry.get()
 
-        # find stream
-        try:
-            self.stream_online = self.stream.find_stream('twitch.tv/'+name)
 
-        finally:
-            self.stream.set_quality('best')
-            if self.stream_online == 0:
-                # offline
-                pass
-            elif self.stream_online == 1:
-                if not self.playing:
-                    self.play()
-                else:
-                    self.stop()
-                    self.play()
-                # online
-            else:
-                # not a valid link
-                pass
 
-        #activate play and pause button
-        self.play_button.config(state=tk.NORMAL)
-        self.pause_button.config(state=tk.NORMAL)
+    def browse_channels(self):
+        pass
+
+    def browse_search(self):
+        # # send streamer name to the livestreamer module
+        # name = self.search_entry.get()
+        pass
+        # # find stream
+        # try:
+        #     self.stream_online = self.stream.find_stream('twitch.tv/'+name)
+
+        # finally:
+        #     self.stream.set_quality('best')
+        #     if self.stream_online == 0:
+        #         # offline
+        #         pass
+        #     elif self.stream_online == 1:
+        #         if not self.playing:
+        #             self.play()
+        #         else:
+        #             self.stop()
+        #             self.play()
+        #         # online
+        #     else:
+        #         # not a valid link
+        #         pass
+
+        # #activate play and pause button
+        # self.play_button.config(state=tk.NORMAL)
+        # self.pause_button.config(state=tk.NORMAL)
 
     # Main Window construction
 
-    def create_top_frame(self):
-        search_frame = tk.Frame(self.root)
-        self.streamer_entry = tk.Entry(search_frame)
-        streamer_button = tk.Button(search_frame, text='search streamer', command=self.search)
-        self.streamer_entry.pack()
-        streamer_button.pack()
-        search_frame.pack()
+    # MainFrame when browsing
+    def create_browsing_frame(self):
+        # left pane with search entry, games button, channel button, follows button, user button
+        pass
+        # mid pane with result : set size for each result, dynamic row/col with window size
+        # last res is offset for more.
+
+    # MainFrame when watching
+    def create_watching_frame(self):
+        # left side pane
+        # create video frame
+        pass
+        # create controller frame
+        # create irc frame
+
+    # def create_top_frame(self):
+    #     search_frame = tk.Frame(self.root)
+    #     self.streamer_entry = tk.Entry(search_frame)
+    #     streamer_button = tk.Button(search_frame, text='search streamer', command=self.search)
+    #     self.streamer_entry.pack()
+    #     streamer_button.pack()
+    #     search_frame.pack()
+
+    def create_left_side_panel(self):
+        # recreate the left panel of twitch:
+        self.left_pane = tk.Frame(self.root, width=150)
+        # - Search
+        self.search_entry = tk.Entry(self.left_pane)
+        self.search_entry.pack(fill=tk.X)
+        search_button = tk.Button(self.left_pane, text='Search', command=self.browse_search)
+        search_button.pack(fill=tk.X)
+        # - Following
+        # - Games
+        games_button = tk.Button(self.left_pane, text='Games', command=self.browse_games)
+        games_button.pack(fill=tk.X, side=tk.TOP)
+        # - Channel
+        channels_button = tk.Button(self.left_pane, text='Channels', command=self.browse_channels)
+        channels_button.pack(fill=tk.X, side=tk.TOP)
+        # - User
+
+        # add left side panel to root
+        self.left_pane.pack(fill=tk.Y, expand=1, side=tk.LEFT)
+        
+       
+
+
+    def create_playing_frame(self):
+        self.play_frame = tk.Frame()
+        # video frame
+        # player and control
+        self.create_player_frame()
+        self.create_bottom_frame()
+        # chat frame
+
 
     def create_player_frame(self):
         player_frame = tk.Frame(self.root, width=500, height=500)
@@ -86,10 +144,11 @@ class GUI():
         self.pause_button.pack()
         bottom_frame.pack()
 
-    def buildUI(self):
-        self.create_top_frame()
-        self.create_player_frame()
-        self.create_bottom_frame()
+    def defaultUI(self):
+        # self.create_top_frame()
+        # self.create_player_frame()
+        # self.create_bottom_frame()
+        self.create_left_side_panel()
 
 
 def main():
