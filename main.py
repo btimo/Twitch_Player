e7f313a4abcf354725d3ed233a7b717bd45bac57
btimo <timo.b35@gmail.com>
2014-11-28 19:29:29 +0100

    - added comments - scroll bar added for the game/channel table list

--- main.py
+++ main.py
@@ -41,33 +41,24 @@ class GUI():
 
 
     def browse_games(self, offset=None):
-        print "browse games"
         # make request for game list
         games_list, next_list = browse_module.get_games(None, offset)
 
         # if watching, stop and destroy play frame
         if self.watching:
-            print "stop watching"
-            self.stop()
-            self.watching = False
-            self.browsing = True
-            for child in self.main_frame.winfo_children():
-                child.destroy()
-            self.create_browsing_panel()
-            self.create_table(games_list, next_list)
-        else:
-            self.create_table(games_list, next_list)
+            self.from_watch_to_browse()
+        # call create_table to make a table with the game list
+        self.create_table(games_list, next_list)
         
 
 
 
     def browse_channels(self, elem, offset=None):
-        print "browse channels"
+        # request the channel list for a specific game
         channel_list, next_list = elem.access_game_streams(offset)
         self.create_table(channel_list, next_list)
 
     def watch_stream(self, elem):
-        print "watch stream"
         self.browsing = False
         self.watching = True
         #clear main_frame
@@ -81,29 +72,32 @@ class GUI():
 
         self.play()
 
+    def from_watch_to_browse(self):
+        self.stop()
+        self.watching = False
+        self.browsing = True
+        # remove all the childs from main_frame
+        for child in self.main_frame.winfo_children():
+            child.destroy()
+        # recreate the title bar and the table frame
+        self.create_browsing_panel()
+
+
 
     def browse_search(self):
-        print 'browse search'
+        # make request with entry string
         channel_list = browse_module.search_request(self.search_entry.get())
-        self.browsing = True
-        self.watching = False
+       
 
+        # if watching
         if self.watching:
-            print "stop watching"
-            self.stop()
-            self.watching = False
-            self.browsing = True
-            for child in self.main_frame.winfo_children():
-                child.destroy()
-            self.create_browsing_panel()
-            self.create_table(channel_list, None)
-        else:
-            self.create_table(channel_list, None)
+            self.from_watch_to_browse
+            
+        self.create_table(channel_list, None)
 
         
     # Main Window construction
     def create_left_side_panel(self):
-        print "left side panel"
         # recreate the left panel of twitch:
         self.left_pane = tk.Frame(self.root, width=150)
         # - Search
@@ -125,49 +119,90 @@ class GUI():
 
     # MainFrame when browsing
     def create_table(self, object_list, next_list):
-        print "create_table"
-        # if browsing game
-        if self.table_frame.winfo_children():
-            for child in self.table_frame.winfo_children():
+        # called to draw a 2D table with games/channel/search result
+
+        # clear the table frame to draw others element
+        if self.table_canvas_frame.winfo_children():
+            for child in self.table_canvas_frame.winfo_children():
                 child.destroy()
+
+        # get dimension of window to put element in the right place
         height = self.table_frame.winfo_height()
         width = self.table_frame.winfo_width()
-        button_width = 136
-        button_height = 190
-        preview_width = 320
-        max_col_preview = width // preview_width
-        max_col = width // button_width
+
+        # constant size of button for
+        # - game list
+        game_img_width = 136
+        game_img_height = 190
+        # - channel list
+        channel_preview_width = 320
+
+        # calc for element by column and row
+        max_col_preview = width // channel_preview_width
+        max_col = width // game_img_width
+
+        # iterator for element position
         i = 0
+
         # make a button for all the games in the list
         for elem in object_list:
+            # get number of element by row (depend of elem type)
             m = max_col if isinstance(elem, browse_module.Game) else max_col_preview
+            # get picture of elem
             pic = elem.pic_medium if isinstance(elem, browse_module.Game) else elem.preview
             im = ImageTk.PhotoImage(Image.open(StringIO(browse_module.request_image(pic))))
-            but = tk.Button(self.table_frame, text=elem.name if isinstance(elem, browse_module.Game) else elem.display_name, image=im, compound=tk.TOP, height=button_height + 50,wraplength=100, command=lambda elem=elem : self.browse_channels(elem) if isinstance(elem, browse_module.Game) else self.watch_stream(elem))
+            # create button for elem
+            but = tk.Button(self.table_canvas_frame, text=elem.name if isinstance(elem, browse_module.Game) else elem.display_name, image=im, compound=tk.TOP, height=game_img_height + 50,wraplength=100, command=lambda elem=elem : self.browse_channels(elem) if isinstance(elem, browse_module.Game) else self.watch_stream(elem))
+            # re set the image parameter to force it to show
             but.image = im
             but.grid(row=(i//m), column=(i%m), sticky=tk.N+tk.S+tk.E+tk.W)
             # tk.Grid.columnconfigure(self.table_frame,i,weight=1, minsize=136)
             # tk.Grid.rowconfigure(self.table_frame,i//max_col, weight=1, minsize=190)
             i += 1
-        # make button for accessing the next games 
+
+        # make button for accessing the next part of the list if there is any
         if next_list != None:
-            tk.Button(self.table_frame, text='Next games', command=lambda: self.browse_games(next_list['offset']) if isinstance(elem, browse_module.Game) 
+            tk.Button(self.table_canvas_frame, text='Next Games' if isinstance(elem, browse_module.Game) else 'Next Channels',height = 10 , command=lambda: self.browse_games(next_list['offset']) if isinstance(elem, browse_module.Game) 
                 else self.browse_channels(elem, next_list['offset'])).grid(row=(i//m), column=(i%m), sticky=tk.N+tk.S+tk.E+tk.W)
+
+        # draw object inside canvas to update its dimension.
+        self.table_canvas_frame.update_idletasks()
+        self.table_canvas.config(scrollregion=self.table_canvas.bbox("all"))
+        
 
 
     def create_browsing_panel(self):
-        print "browsing panel"
-        # left pane with search entry, games button, channel button, follows button, user button
+        # title bar for section title (All games/Game name) and search entry for specific element in the table
         self.title_bar = tk.Frame(self.main_frame)
+        # title
         self.title_bar_title_label = tk.Label(self.title_bar, text="All games")
         self.title_bar_title_label.pack(side=tk.LEFT)
+        # entry
         self.title_bar_search_entry = tk.Entry(self.title_bar)
         self.title_bar_search_entry.pack(side=tk.RIGHT)
+
         self.title_bar.pack(side=tk.TOP, fill=tk.X) 
-        # mid pane with result : set size for each result, dynamic row/col with window size
-        # last res is offset for more.
-        # call function to create the table
+
+        # table to contain the elements
         self.table_frame = tk.Frame(self.main_frame)
+        # give importance to the canvas
+        self.table_frame.grid_rowconfigure(0, weight=1)
+        self.table_frame.grid_columnconfigure(0, weight=1)
+        
+
+        # create the scrollbar
+        yscrollbar = tk.Scrollbar(self.table_frame)
+        yscrollbar.grid(row=0, column=1, sticky=tk.N+tk.S)
+
+        self.table_canvas = tk.Canvas(self.table_frame, yscrollcommand=yscrollbar.set, bg='black')
+        self.table_canvas.grid(row=0, column=0, sticky = tk.N+tk.S+tk.E+tk.W)
+
+        self.table_canvas_frame = tk.Frame(self.table_canvas)
+
+        self.table_canvas.create_window(0, 0, anchor=tk.N + tk.W, window=self.table_canvas_frame)
+
+        yscrollbar.config(command=self.table_canvas.yview)
+
         self.table_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
         self.root.update()
@@ -175,29 +210,31 @@ class GUI():
 
     # MainFrame when watching
     def create_playing_frame(self):
-        self.play_frame = tk.Frame()
-        # video frame
-        # player and control
+        # create player_frame
         self.create_player_frame()
-        # self.create_bottom_frame()
-        # chat frame
+        # create overlay
 
 
     def create_player_frame(self):
-        # setting empty bg fix flickering
+        # create frame for the player
+        # set bg to "" fix flickering
         player_frame = tk.Frame(self.main_frame, bg='')
         player_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        # get frame id for gstreamer sync-message
         self.player_frame_id = player_frame.winfo_id()
 
 
     def defaultUI(self):
-        print "default ui"
+        # create left side panel
         self.create_left_side_panel()
-        print "create main frame"
+        # create main_frame (title, table, player)
         self.main_frame = tk.Frame(self.root)
-        self.main_frame.pack_propagate(0)
         self.main_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        # size not controlled by child
+        self.main_frame.pack_propagate(0)
+        # create browsing panel (title, table)
         self.create_browsing_panel()
+        # browse game once to initialize the table
         self.browse_games()
 
 
